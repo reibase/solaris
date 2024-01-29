a3381f76981c65529f6f99c5b96e488d94a2f2fc
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-01-29 12:44:49 -0500

    unnecessary change reverted

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import axios from "axios";
 function App() {
-	const auth = async () => {
-		console.log("clicked");
-		await axios.get("/auth/github").then(function (res) {
-			console.log("res", res);
-		});
-	};
 	return (
 		<div className="text-3xl font-bold underline text-blue-800">
-			<a href="/auth/github">Hello world</a>
+			Hello world
 		</div>
 	);
 }
