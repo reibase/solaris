bf6dc6b74c6ec838c6d72babf091c019c32e8b82
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-01-29 12:15:44 -0500

    passport github strategy

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import axios from "axios";
 function App() {
-
-  return (
-      <div className="text-3xl font-bold underline text-blue-800">
-        Hello world
-      </div>
-  )
+	const auth = async () => {
+		console.log("clicked");
+		await axios.get("/auth/github").then(function (res) {
+			console.log("res", res);
+		});
+	};
+	return (
+		<div className="text-3xl font-bold underline text-blue-800">
+			<a href="/auth/github">Hello world</a>
+		</div>
+	);
 }
 
-export default App
+export default App;
