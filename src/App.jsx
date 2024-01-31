4d36ac7db41472f6bc856d26a875c7e8b87cfaa0
Cristian <125297328+ramirc5@users.noreply.github.com>
2024-01-31 10:17:54 -0800

    first commit on router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import Login from './components/Login.jsx'
+import RequestAccess from './components/RequestAccess.jsx'
+import Profile from './components/Profile.jsx'
+
 function App() {
-	return (
-		<div className="text-3xl font-bold underline text-blue-800">
-			Hello world
-		</div>
-	);
+
+  return (
+      <>
+        <Profile/>
+      </>
+  )
 }
 
 export default App;
