aaaf4caebd0bedac1cfaae4af22552194883c547
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-01-31 08:50:25 -0500

    pulling out ssr

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-  },
-})
+	plugins: [react()],
+	server: { proxy: {"/": "http://localhost:3000/"} },
+});
