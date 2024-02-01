74b8bcb37cfca017bee741b84073da8d8d1e3098
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-02-01 13:49:16 -0500

    removed console log

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import "dotenv/config";
-console.log(process.env.NODE_ENV)
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/",
