6e49c304b7528631e420c8bb40485068c2ab439d
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-02-02 16:57:32 -0500

    comments added to main, login, and profile.jsx

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -3,40 +3,51 @@ import githubLogo from "../assets/github-logo.png";
 import gmailLogo from "../assets/gmail-logo.png";
 import { Link } from "react-router-dom";
 
-// query endpoint on clicking github button, which makes a GET request to /api/auth/github
+// To do:
+// 1. a href links need to point to serivce, github etc, not 'etc'
+// 2. add gitlab button, with endpoint /api/auth/gitlab
+// 3. Practice signing in to each service, log out of GitHub or Google to make sure it works because it
+// redirects to "/" so it's hard to tell what happened.
 
 const Login = () => {
-  return (
-    <>
-      <Nav />
-      <div className="mx-auto bg-white shadow-lg rounded-lg flex flex-col items-center py-[50px] w-5/6 md:w-2/3 lg:w-1/2">
-        <h1 className="font-inter mb-[70px] text-3xl font-bold underline text-center">
-          Solaris Logo
-        </h1>
-        <div class="flex flex-col gap-[20px]">
-          <a
-            href="/api/auth/etc"
-            className="flex gap-[10px] w-[300px] px-[20px] items-center w-full rounded-md border border-black"
-          >
-            <span className="font-inter">Continue with GitHub</span>
-            <img src={githubLogo} />
-          </a>
-          <a
-            href="/api/auth/etc"
-            className="flex gap-[10px] w-[300px] px-[20px] items-center justify-space-between w-full rounded-md border border-black"
-          >
-            <span className="font-inter">Continue with Gmail</span>
-            <img src={gmailLogo} />
-          </a>
-        </div>
-        <Link to="/requestaccess">
-          <button class="font-inter mt-[30px] mx-auto bg-[#313131] w-[350px] px-[20px] text-white rounded-md px-4 py-[3px]">
-            Request Access
-          </button>
-        </Link>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Nav />
+			<div className="mx-auto bg-white shadow-lg rounded-lg flex flex-col items-center py-[50px] w-5/6 md:w-2/3 lg:w-1/2">
+				<h1 className="font-inter mb-[70px] text-3xl font-bold underline text-center">
+					Solaris Logo
+				</h1>
+				<div class="flex flex-col gap-[20px]">
+					<a
+						href="/api/auth/github"
+						className="flex gap-[10px] w-[300px] px-[20px] items-center w-full rounded-md border border-black"
+					>
+						<span className="font-inter">Continue with GitHub</span>
+						<img src={githubLogo} />
+					</a>
+					<a
+						href="/api/auth/etc"
+						className="flex gap-[10px] w-[300px] px-[20px] items-center justify-space-between w-full rounded-md border border-black"
+					>
+						<span className="font-inter">Continue with GitLab</span>
+						<img src={gmailLogo} />
+					</a>
+					<a
+						href="/api/auth/etc"
+						className="flex gap-[10px] w-[300px] px-[20px] items-center justify-space-between w-full rounded-md border border-black"
+					>
+						<span className="font-inter">Continue with Google</span>
+						<img src={gmailLogo} />
+					</a>
+				</div>
+				<Link to="/requestaccess">
+					<button class="font-inter mt-[30px] mx-auto bg-[#313131] w-[350px] px-[20px] text-white rounded-md px-4 py-[3px]">
+						Request Access
+					</button>
+				</Link>
+			</div>
+		</>
+	);
 };
 
 export default Login;
--- src/client/main.jsx
+++ src/client/main.jsx
@@ -8,14 +8,17 @@ import AccessCode from "./components/AccessCode.jsx";
 import RequestAccess from "./components/RequestAccess.jsx";
 import Thanks from "./components/Thanks.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
+// new endpoint: should run on load:
+// get /api/auth/me -> if user is logged in, returns a user obj, else returns 404. set isLogged in to true if user obj
+// change / route to be loging if not logged in and profile if logged in
 const router = createBrowserRouter([
 	{ path: "/", element: <AccessCode /> },
-	{ path: "/profile", element: <Profile /> },
-	{ path: "/requestaccess", element: <RequestAccess /> },
+	{ path: "/profile", element: <Login /> },
+	{ path: "/requestaccess", element: <Login /> },
 	{ path: "/login", element: <Login /> },
 	{ path: "/access", element: <AccessCode /> },
 	{ path: "/thanks", element: <Thanks /> },
