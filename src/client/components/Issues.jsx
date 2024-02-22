99edd206bd9d80ea767f529250d16b9344a70d5a
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-02-22 11:19:52 -0500

    single project page styled

--- src/client/components/Issues.jsx
+++ src/client/components/Issues.jsx
@@ -4,7 +4,8 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 import githubLogo from "../assets/github.svg";
-import githubDarkmode from "../assets/github-darkmode.svg";
+import githubLogoDarkMode from "../assets/github-darkmode.svg";
+import gitlabLogo from "../assets/gitlab.svg";
 import { useStore } from "../store.js";
 import ExternalLink from "../assets/ExternalLink.svg";
 import darkDataTransfer from "../assets/darkDataTransfer.svg";
@@ -17,7 +18,10 @@ import ProgressBar from "./ProgressBar.jsx";
 export default function Projects() {
 	const { dark } = useStore();
 	let { id } = useParams();
-
+	const icon = {
+		github: dark ? githubLogoDarkMode : githubLogo,
+		gitlab: gitlabLogo,
+	};
 	const getProject = async () => {
 		try {
 			const { data } = await axios
@@ -35,71 +39,71 @@ export default function Projects() {
 	console.log(id, data);
 
 	return (
+		// wrapper
 		<div className="w-full h-full flex flex-col gap-[10px]">
-			<div className="w-full shadow-lg rounded-lg text-sm flex flex-col items-center bg-white/90 dark:bg-[#202530] border border-transparent border-1 dark:border-[#373D47] justify-between gap-[10px]">
-				<div className="flex flex-row w-full justify-between p-4">
-					<div className="flex flex-row">
-						<div className="flex flex-col gap-[15px]">
-							<div className="flex flex-col">
-								<div className="flex gap-[10px]">
-									<h2 className="font-semibold dark:text-white">
-										{data?.title}
-									</h2>
-									<span className="font-semibold bg-[#EEFDF2] px-[15px] rounded-md text-[#1C7737] dark:bg-[#185B2E] dark:text-[#7FEDA2]">
-										LIVE
-									</span>
-								</div>
-								<span className="font-light text-[#313131] dark:text-[#8B929F]">
-									Added on January 24
+			{/* header */}
+			<div className="w-full h-60 items-start justify-start p-4 shadow-md rounded-lg text-sm flex flex-col bg-white/90 dark:bg-[#202530] border border-transparent border-1 dark:border-[#373D47]">
+				{/* top row of header */}
+				<div className="flex flex-row w-full justify-between">
+					{/* top left of header */}
+					<div className="flex flex-row gap-[20px]">
+						<span className="font-semibold dark:text-white">{data?.title}</span>
+						<span className="flex items-center justify-center font-semibold bg-[#EEFDF2] text-[10px] px-[10px] h-[18px] rounded-md text-[#1C7737] dark:bg-[#185B2E] dark:text-[#7FEDA2]">
+							{data?.live ? `LIVE` : `TEST`}
+						</span>
+					</div>
+					{/* top right of header */}
+					<span className="text-[12px] font-semibold dark:text-[#DDDCDC] whitespace-nowrap">
+						50000 Credits
+					</span>
+				</div>
+
+				<span className="mt-2 text-[#313131] dark:text-[#8B929F]">
+					Added on {data?.createdAt.slice(0, 10)}
+				</span>
+
+				{/* bottom row of header  */}
+				<div className="flex flex-row h-full items-end flex-wrap gap-[15px]">
+					<a href={data?.url} target="_blank">
+						<div className="flex border border-[#8D4D4D4] dark:border-[#8B929F] rounded-md py-[2px] px-[12px] w-[180px] md:w-[240px] justify-between items-center">
+							<div className="flex gap-[10px]">
+								<img className="w-[14px]" src={icon[data?.host]} />
+								<span className="dark:text-[#8B929F] text-[11px] w-[135px] text-left truncate overflow-hidden">
+									{data?.identifier}
 								</span>
 							</div>
-							<div className="flex flex-row flex-wrap gap-[15px]">
-								<button className="flex border border-[#919190] dark:border-[#8B929F] rounded-md text-[10px] px-[12px] w-[180px] md:w-[220px] justify-between items-center">
-									<div className="flex gap-[10px]">
-										<img
-											className="w-[14px]"
-											src={dark ? githubDarkmode : githubLogo}
-										/>
-										<span className="font-semibold dark:text-white">
-											repo-name on GitHub
-										</span>
-									</div>
-									<img src={dark ? darkExternalLink : ExternalLink} />
-								</button>
-								<div className="flex items-center gap-[20px]">
-									<div className="flex gap-[5px]">
-										<img className="w-[20px]" src={dark ? darkGroup : Group} />
-										<p className="text-[#313131] dark:text-[#D9D9D9] text-[12px] font-medium">
-											Community
-										</p>
-									</div>
-									<div className="flex gap-[5px]">
-										<img src={darkDataTransfer} />
-										<p className="text-[#313131] dark:text-[#D9D9D9] text-[12px] font-medium">
-											Transfer
-										</p>
-									</div>
-									<div className="flex gap-[5px]">
-										<img src={darkSettings} />
-										<p className="text-[#313131] dark:text-[#D9D9D9] text-[12px] font-medium">
-											Settings
-										</p>
-									</div>
-								</div>
-							</div>
+							<img src={dark ? darkExternalLink : ExternalLink} />
+						</div>
+					</a>
+
+					<div className="flex items-center gap-[20px]">
+						<div className="flex gap-[7px]">
+							<img className="w-[20px]" src={dark ? darkGroup : Group} />
+							<p className="text-[#313131] dark:text-[#D9D9D9] text-[12px] font-medium">
+								Community
+							</p>
+						</div>
+						<div className="flex gap-[7px]">
+							<img src={darkDataTransfer} />
+							<p className="text-[#313131] dark:text-[#D9D9D9] text-[12px] font-medium">
+								Transfer
+							</p>
+						</div>
+						<div className="flex gap-[7px]">
+							<img src={darkSettings} />
+							<p className="text-[#313131] dark:text-[#D9D9D9] text-[12px] font-medium">
+								Settings
+							</p>
 						</div>
 					</div>
-					<p className="text-[12px] font-semibold dark:text-[#DDDCDC] whitespace-nowrap">
-						50000 Credits
-					</p>
 				</div>
 			</div>
 
-			<div className="w-full p-4 shadow-lg rounded-lg text-sm flex flex-col items-center md:px-[40px] bg-white/90 dark:bg-[#202530] border border-transparent border-1 dark:border-[#373D47] justify-between overflow-auto">
-				<div className="w-full h-full overflow-auto pr-6">
+			<div className="w-full pl-2 shadow-lg rounded-lg text-sm flex flex-col items-center bg-white/90 dark:bg-[#202530] border border-transparent border-1 dark:border-[#373D47] justify-between overflow-auto">
+				<div className="w-full h-full overflow-auto pr-4">
 					{data &&
 						data?.Issues.map((pullRequest, index) => (
-							<div className="flex flex-row w-full justify-between border-b border-[#D4D4D4] pb-4 mb-6 hover:bg-[#161f2d]">
+							<div className="flex flex-row w-full justify-between border-b border-[#D4D4D4] p-3 pr-1 mt-2 mb-1 hover:bg-slate-100/25 dark:hover:bg-[#161f2d] dark:border-[#373D47]">
 								<div className="flex flex-row">
 									<div className="flex flex-col gap-[25px]">
 										<div className="flex flex-col">
@@ -108,22 +112,22 @@ export default function Projects() {
 													{pullRequest.title}
 												</h2>
 											</div>
-											<span className="font-light text-[#313131] dark:text-[#8B929F]">
+											<span className="text-gray-400 mt-1 dark:text-[#8B929F]">
 												#{pullRequest.number} opened on May 23 by ramirc5
 											</span>
 										</div>
-										<button className="flex border border-[#919190] dark:border-[#8B929F] rounded-md text-[10px] px-[12px] w-[180px] md:w-[220px] justify-between items-center gap-[5px]">
-											<div className="flex gap-[10px]">
-												<img
-													className="w-[14px]"
-													src={dark ? githubDarkmode : githubLogo}
-												/>
-												<span className="font-semibold max-w-[125px] text-ellipsis overflow-hidden text-nowrap dark:text-white">
-													{pullRequest.title}
-												</span>
+
+										<a href={pullRequest?.url} target="_blank">
+											<div className="flex border border-[#D4D4D4] dark:border-[#8B929F] rounded-md text-[10px] px-[12px] w-[180px] md:w-[220px] justify-between items-center gap-[5px]">
+												<div className="flex gap-[10px]">
+													<img className="w-[14px]" src={icon[data?.host]} />
+													<span className="font-semibold max-w-[125px] text-ellipsis overflow-hidden text-nowrap dark:text-white">
+														{pullRequest.title}
+													</span>
+												</div>
+												<img src={dark ? darkExternalLink : ExternalLink} />
 											</div>
-											<img src={dark ? darkExternalLink : ExternalLink} />
-										</button>
+										</a>
 									</div>
 								</div>
 								<div className="flex flex-col justify-end gap-[10px] md:gap-0 items-end md:justify-between ">
