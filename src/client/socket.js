8978452df0817b0831b05b75f0c414081433060f
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-03-01 15:41:36 -0500

    transports

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io();
+const socket = io("http://localhost:3001/", {
+	transports: ["websocket"],
+});
 
 // client-side
 socket.on("connect", () => {
