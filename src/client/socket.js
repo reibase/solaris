d80344a2d3879332ca2d91ead05d23db3e43d6cc
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-03-01 15:20:07 -0500

    url removed

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,10 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://seashell-app-fs86p.ondigitalocean.app/", {
-	transports: ["websocket"],
-	secure: true,
-	rejectUnauthorized: false,
-});
+const socket = io();
 
 // client-side
 socket.on("connect", () => {
