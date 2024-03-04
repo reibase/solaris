5825745c8d3e05bf726a7bc9b3ededc7f8ed234a
Jeffrey Wood <jeffreywood@Jeffreys-Air.home>
2024-03-04 15:04:21 -0500

    logging socket error

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,12 +1,16 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001/", {
+const socket = io("https://seashell-app-fs86p.ondigitalocean.app", {
 	transports: ["websocket"],
 });
 
 // client-side
 socket.on("connect", () => {
 	console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.on("connect_error", (err) => {
+	console.log(`connect_error due to ${err.message}`);
 });
 
 socket.on("disconnect", () => {
